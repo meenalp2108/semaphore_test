6ac947f6dac058db19e24efe51df5c74e3a38894
meenalp2108 <68324038+meenalp2108@users.noreply.github.com>
2020-11-19 19:04:14 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -15,14 +15,14 @@ def new_function():
 
 @app.route('/get_data_test_val', methods=['POST'])
 def get_data_test_val():
-    data = (request.get_data()).decode()
-    request_input_data = json.loads(data)
-    db_string = 'Driver=SQL Server;Server=15.206.26.2;Database=PSS_SEGMENT4_Aug2020;uid=service;pwd=service@123;Trusted_Connection=no;integratedSecurity=false'
-    conn = pyodbc.connect(db_string)
-    cursor = conn.cursor()
-    cursor.execute('select min(starttime) from AugmentedStateTransactions')
-    return_list = list(cursor.fetchall())
-    return return_list
+    #data = (request.get_data()).decode()
+    #request_input_data = json.loads(data)
+    #db_string = 'Driver=SQL Server;Server=15.206.26.2;Database=PSS_SEGMENT4_Aug2020;uid=service;pwd=service@123;Trusted_Connection=no;integratedSecurity=false'
+    #conn = pyodbc.connect(db_string)
+    #cursor = conn.cursor()
+    #cursor.execute('select min(starttime) from AugmentedStateTransactions')
+    #return_list = list(cursor.fetchall())
+    return 'ok'
 
 if __name__ == "__main__":
    app.run(debug=False, port=4000, host='0.0.0.0')
