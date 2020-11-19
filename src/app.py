c78067b92362ed3e1b7c69157bcc505204fac626
meenalp2108 <68324038+meenalp2108@users.noreply.github.com>
2020-11-19 15:17:24 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 app = Flask(__name__)
 @app.route("/")
 
@@ -9,6 +10,16 @@ def index():
 def new_function():
     return 2 + 3
 
+@app.route('/get_data_test_val', methods=['POST'])
+def get_data_test_val():
+    data = (request.get_data()).decode()
+    request_input_data = json.loads(data)
+    db_string = 'Driver=SQL Server;Server=15.206.26.2;Database=PSS_SEGMENT4_Aug2020;uid=service;pwd=service@123;Trusted_Connection=no;integratedSecurity=false'
+    conn = pyodbc.connect(db_string)
+    cursor = conn.cursor()
+    cursor.execute('select min(starttime) from AugmentedStateTransactions')
+    return_list = list(cursor.fetchall())
+return return_list
 
 if __name__ == "__main__":
     app.run()
