6fecabfaa05d5a0951d1465a712b1448fd4aff84
meenalp2108 <68324038+meenalp2108@users.noreply.github.com>
2020-11-19 15:19:30 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,4 +8,15 @@ def test_index():
 def test_new_function():
     assert new_function() == 5
 
+import requests
+
+params = {
+        "a" : 5,
+        "b" : 7,
+        "n" : 120
+    }
+
+resp = requests.post('http://10.1.0.4:4000/test_service_on_db', json=params)
+print(resp)
+print(resp.json())
 
