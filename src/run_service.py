f532c21c456f2d61ba0267939c8986f4161f5018
meenalp2108 <68324038+meenalp2108@users.noreply.github.com>
2020-11-20 12:07:38 +0530

    Update run_service.py

--- src/run_service.py
+++ src/run_service.py
@@ -1,5 +1,5 @@
 from gevent.pywsgi import WSGIServer
 from src.app import app
 
-http_server = WSGIServer(('172.16.55.145',4000), app)
+http_server = WSGIServer(('172.16.55.142',4000), app)
 http_server.serve_forever()
